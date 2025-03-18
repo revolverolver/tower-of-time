f417f828a155d04fd297945d2751810340ebff40
revolverolver <40372353+revolverolver@users.noreply.github.com>
2025-03-18 20:33:37 +0100

    Added Loading panel

---
+++ Scripts/CanvasController.ts
@@ -0,0 +1,14 @@
+
+import { MonoBehaviour } from "UnityEngine";
+export default class CanvasController extends MonoBehaviour {
+    
+    //Called when script instance is loaded
+    private Awake() : void {}
+
+    //Start is called on the frame when a script is enabled just 
+    //before any of the Update methods are called the first time.
+    private Start() : void {}
+
+    //Update is called every frame, if the MonoBehaviour is enabled.
+    private Update() : void {}
+}
